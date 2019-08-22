c67bf21ac74171350d1ce90d9d145048b583ac4b
Ye Liu <ye.liu@hp.com>
2019-08-22 18:49:49 +0800

    update the json payload

--- pkg/image/anchore/scanner.go
+++ pkg/image/anchore/scanner.go
@@ -58,12 +58,10 @@ func (s *imageScanner) Scan(req harbor.ScanRequest) (*harbor.ScanResponse, error
 	request := gorequest.New().SetBasicAuth(s.cfg.ScannerUsername, s.cfg.ScannerPassword)
 
 	var scannerAPI = s.cfg.ScannerAddress + "/images"
-	log.Printf("anchore-engine add image URL: %s ...", scannerAPI)
+	log.Printf("anchore-engine add image URL: %s", scannerAPI)
 
-	imageToScanReq := &anchore.ScanImagePostReq{
-		P_tag: imageToScan,
-	}
-	log.Printf("anchore-engine add image payload: %s ...", imageToScanReq)
+	var imageToScanReq = `{"tag":"` + imageToScan + `"}`
+	log.Printf("anchore-engine add image payload: %s", imageToScanReq)
 
 	resp, _, _ := request.Post(s.cfg.ScannerAddress + "/images").Send(imageToScanReq).End(checkStatus)
 
